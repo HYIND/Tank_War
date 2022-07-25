ad05690255099eb7ddd9a7319d95f842290a057a
HYIND <1255231699@qq.com>
2022-07-26 05:06:26 +0800

    补充提交

--- source/Game_Component.h
+++ source/Game_Component.h
@@ -2,6 +2,11 @@
 
 using namespace std;
 
+#define BRICK_WIDTH 56
+#define BRICK_HEIGHT 56
+#define IRON_WIDTH 56
+#define IRON_HEIGHT 56
+
 class Game_Component
 {
 	friend class Room_Process;
@@ -22,12 +27,12 @@ class Brick_Wall :public Game_Component
 {
 public:
 	Brick_Wall(int x, int y, int id, int health = 60)
-		:Game_Component(x, y, 50, 50, id, health) {};
+		:Game_Component(x, y, BRICK_WIDTH, BRICK_HEIGHT, id, health) {};
 };
 
 class Iron_Wall :public Game_Component
 {
 public:
 	Iron_Wall(int x, int y, int id, int health = 99999999)
-		:Game_Component(x, y, 50, 50, id, health) {};
+		:Game_Component(x, y, IRON_WIDTH, IRON_HEIGHT, id, health) {};
 };
