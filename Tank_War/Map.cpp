1fa677e0bc167e0c985dd20a98b2dd53c3771114
HYIND <1255231699@qq.com>
2022-07-26 05:06:44 +0800

    补充提交

--- Tank_War/Map.cpp
+++ Tank_War/Map.cpp
@@ -1,5 +1,7 @@
 #include "Map.h"
 
+extern RECT _rect;
+
 int Cur_Map_id = 0;
 map<int, Map> Map_list;
 #define SHOW(Vec) for (auto& v : Vec){v.Draw();}
@@ -16,10 +18,12 @@ void Init_Map_Zero()
 	LoadResourceBitmap(hInst, pIWICFactory, pRenderTarget, L"JPG", MAKEINTRESOURCE(BK_SAND), &Zero_BK);
 	Default_Map.BK_pBitmap = Zero_BK;
 
-	Default_Map.Init_Location.emplace_back(1, 50, 300, UP, TankStyle::DEFAULT);
-	Default_Map.Init_Location.emplace_back(2, 500, 300, UP, TankStyle::DEFAULT);
+	Default_Map.Init_Location.emplace_back(1, 50, 280, UP, TankStyle::DEFAULT);
+	Default_Map.Init_Location.emplace_back(2, _rect.right - 50, 280, UP, TankStyle::DEFAULT);
 
-	//int brick_count = 1;
+	int brick_count = 1;
+	int icon_count = 1;
+
 	//for (int i = 0; i < 10; i++)
 	//{
 	//	Brick_Wall brick(250 + i * BRICK_WIDTH, 150, brick_count);
@@ -34,32 +38,55 @@ void Init_Map_Zero()
 	//	brick_count++;
 	//}
 
-	int icon_count = 1;
 	for (int i = 0; i < 12; i++)
 	{
-		if (i == 5 || i == 6) continue;
-		Iron_Wall iron(300 + i * IRON_WIDTH, 100, icon_count);
+		if (i == 5 || i == 6)
+		{
+			Brick_Wall brick(280 + i * IRON_WIDTH, 110, brick_count);
+			Default_Map.Brick_info.emplace_back(brick);
+			brick_count++;
+			continue;
+		}
+		Iron_Wall iron(280 + i * IRON_WIDTH, 110, icon_count);
 		Default_Map.Iron_info.emplace_back(iron);
 		icon_count++;
 	}
 	for (int i = 0; i < 12; i++)
 	{
-		if (i == 5 || i == 6) continue;
-		Iron_Wall iron(300 + i * IRON_WIDTH, 100 + 7 * IRON_HEIGHT, icon_count);
+		if (i == 5 || i == 6)
+		{
+			Brick_Wall brick(280 + i * IRON_WIDTH, 110 + 7 * IRON_HEIGHT, brick_count);
+			Default_Map.Brick_info.emplace_back(brick);
+			brick_count++;
+			continue;
+		}
+		Iron_Wall iron(280 + i * IRON_WIDTH, 110 + 7 * IRON_HEIGHT, icon_count);
 		Default_Map.Iron_info.emplace_back(iron);
 		icon_count++;
 	}
 	for (int i = 1; i < 7; i++)
 	{
-		if (i == 3 || i == 4) continue;
-		Iron_Wall iron(300, 100 + i * IRON_HEIGHT, icon_count);
+		if (i == 3 || i == 4)
+		{
+			Brick_Wall brick(280, 110 + i * IRON_HEIGHT, brick_count);
+			Default_Map.Brick_info.emplace_back(brick);
+			brick_count++;
+			continue;
+		}
+		Iron_Wall iron(280, 110 + i * IRON_HEIGHT, icon_count);
 		Default_Map.Iron_info.emplace_back(iron);
 		icon_count++;
 	}
 	for (int i = 1; i < 7; i++)
 	{
-		if (i == 3 || i == 4) continue;
-		Iron_Wall iron(300 + 11 * IRON_WIDTH, 100 + i * IRON_HEIGHT, icon_count);
+		if (i == 3 || i == 4)
+		{
+			Brick_Wall brick(280 + 11 * IRON_WIDTH, 110 + i * IRON_HEIGHT, brick_count);
+			Default_Map.Brick_info.emplace_back(brick);
+			brick_count++;
+			continue;
+		}
+		Iron_Wall iron(280 + 11 * IRON_WIDTH, 110 + i * IRON_HEIGHT, icon_count);
 		Default_Map.Iron_info.emplace_back(iron);
 		icon_count++;
 	}
@@ -67,7 +94,7 @@ void Init_Map_Zero()
 	{
 		for (int j = 3; j < 5; j++)
 		{
-			Iron_Wall iron(300 + i * IRON_WIDTH, 100 + j * IRON_HEIGHT, icon_count);
+			Iron_Wall iron(280 + i * IRON_WIDTH, 110 + j * IRON_HEIGHT, icon_count);
 			Default_Map.Iron_info.emplace_back(iron);
 			icon_count++;
 		}
