a59e268b9a5aee1053ff21882456d6f7ea8ebabb
HYIND <1255231699@qq.com>
2026-02-27 19:27:01 +0800

    补充

--- Client/Tank_War/resource.h
+++ Client/Tank_War/resource.h
@@ -5,6 +5,8 @@
 #define IDC_MYICON                      2
 #define IDS_APP_TITLE                   103
 #define IDD_ABOUTBOX                    103
+#define Default_Shoot                   103
+#define Default_Shoot_Audio             103
 #define IDM_ABOUT                       104
 #define IDC_POINTER                     104
 #define IDM_EXIT                        105
@@ -29,12 +31,17 @@
 #define TANK_PNG                        177
 #define PAUSE_PNG                       186
 #define BRICK_WALL                      190
-#define HEALTH_KIT                      193
 #define AID_KIT                         193
 #define SNOW                            195
+#define Heal_Audio                      195
 #define WATER                           196
 #define BK_SAND                         197
 #define TANK_GREEN                      203
+#define IDR_ACCELERATOR1                209
+#define IDR_AUDIO1                      213
+#define Default_Attacked_Audio          215
+#define GAME_BGM_AUDIO                  217
+#define MENU_BGM_AUDIO                  218
 #define IDC_BUTTON1                     1002
 #define TANK_BLUE                       1002
 #define IDC_BUTTON2                     1003
@@ -54,8 +61,8 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        204
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_RESOURCE_VALUE        219
+#define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         1021
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
